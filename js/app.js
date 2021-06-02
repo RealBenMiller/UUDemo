fc6f2687abd77371e4b2b4d64555130ab4568afe
RealBenMiller <b.r.miller@hotmail.co.uk>
2021-06-02 12:39:27 +0100

    initial commit

---
+++ js/app.js
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2016 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+"use strict";
+
+// Creating hotspot locations
+
+scene.hotspotContainer().createHotspot(document.querySelector("#HSSewer"), {
+  "yaw": 0.5486307201407392,
+          "pitch": -0.06210568266343586,
+});
+
+scene.hotspotContainer().createHotspot(document.querySelector("#HSLeaflet"), {
+  "yaw": -0.009800782442782818,
+          "pitch": 0.07867625306405479,
+});
+
+scene.hotspotContainer().createHotspot(document.querySelector("#HSImages"), {
+  "yaw": 1.0249288057263612,
+          "pitch": -0.1637653141985318,
+});
+
+scene.hotspotContainer().createHotspot(document.querySelector("#HSText"), {
+  "yaw": 0.49215228092795194,
+          "pitch": -0.07333686974735798,
+});
+
+scene.hotspotContainer().createHotspot(document.querySelector("#HSQuestions"), {
+  "yaw": 0.6640966143557137,
+          "pitch": 0.17053384631492285,
+});
+
+scene.hotspotContainer().createHotspot(document.querySelector("#HSFatberg"), {
+  "yaw": 2.258145934553837,
+          "pitch": 0.051891414247325685,
+});
+
+scene.hotspotContainer().createHotspot(document.querySelector("#HSVideo"), {
+  "yaw": -1.5299419121833928,
+          "pitch": 0.09864333983462004,
+});
+
+
+// MODALS
